320abdba321f12d3ea01b15a93a8ff6b6b2a0e61
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-09-09 18:16:49 -0400

    update

---
+++ operators/5-equality-operators/index.js
@@ -0,0 +1,8 @@
+
+// Strict Equality Operators (Same type and value)
+// console.log(1 === 1);
+// console.log('1' === 1);
+
+//Lose Equality Operators (Only checks if the values are equal)
+console.log(1 == 1);
+console.log('1 == 1');
