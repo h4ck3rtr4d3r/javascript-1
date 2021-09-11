91d1fc6b84e937f5a0e1b420b174ef55c2d3f7d4
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-09-11 17:49:56 -0400

    update

---
+++ operators/exercise-swapping variables/index.js
@@ -0,0 +1,12 @@
+let a ='red';
+let b = 'blue';
+
+let c = a;
+a = b; // here a is reasign with the value of b
+b = c; // here b is reassign with the value of c
+
+console.log(a);
+console.log(b);
+
+
+
