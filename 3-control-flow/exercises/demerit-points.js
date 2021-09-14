7c4037f2e85fd477920ac3d488e3b48a19d84e52
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-09-14 14:16:22 -0400

    update

---
+++ 3-control-flow/exercises/demerit-points.js
@@ -0,0 +1,21 @@
+// Speed Limit = 70
+// 5b -> 1 point
+// Math.floor(1.3)
+// 12 poist -> suspended
+
+checkspeed(130);
+
+function checkspeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+
+  if (speed < speedLimit + kmPerPoint){
+      console.log("Ok");
+      return;
+    }
+     
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12) console.log("License suspended");
+    else console.log("Points", points);
+  }
+}
