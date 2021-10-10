965b906d079e6eec148ebdb2200cfd8cd1fb76b3
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-10-10 15:56:20 -0400

    update

--- 1-basics/8-types-of-functions/index.js
+++ 1-basics/8-types-of-functions/index.js
@@ -1,6 +1,12 @@
-// Calculating a value
+// Function that is performe a task
+function greet(personName, lastName) {
+  console.log(`Hello ${personName} ${lastName}`);
+}
+greet("Yosef", "Oliveira");
+greet("Leah", "Oliveira");
+
+// Function that calculate a value
 function square(number) {
   return number * number;
 }
-
 console.log(square(6));
